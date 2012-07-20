b91e733a7d9340668509712057f3e3059152a5b5
akshay-vishnoi <akshay.vishnoi@vinsol.com>
2012-07-20 18:19:30 +0530

    9th finally done

---
+++ exercise-9/js/load.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	
+        $('#blog a').click(function(e) {
+                e.preventDefault();
+        });
+
+	$('#blog').find('h3').each(function(index,element) {
+		var div = $('<div></div>'), current_h3 = $(this), h3_a_Href = 'data/' + current_h3.find('a').attr('href');
+		h3_a_Href = h3_a_Href.replace('#', ' #');
+		current_h3.after(div);
+		current_h3.data("test", h3_a_Href);
+	});
+
+	$('h3').live('click', function() {
+		var clikd_h3 = $(this);
+		clikd_h3.next('div').load(clikd_h3.data("test"));
+		
+	});
+
+});
