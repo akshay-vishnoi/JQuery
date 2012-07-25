3672055482ae3e68acd7e8dc7709f5c1611b9b41
akshay-vishnoi <akshay.vishnoi@vinsol.com>
2012-07-25 16:24:15 +0530

    element node

--- accordian_implementation/accordion.js
+++ accordian_implementation/accordion.js
@@ -1,18 +1,22 @@
 $(document).ready(function() {
-	var val, address;
+	var val, address, aUnderContainer = $('.container a');;
 	address = location.href;
 	val = address.split("?");
 
 	if(val[1] != undefined) {
-		$($('.container a')[val[1]]).css('background-color', 'grey');
-		$($('a')[val[1]]).parents().slideDown();
+
+		$(aUnderContainer[val[1]]).css('background-color', 'grey');
+		$(aUnderContainer[val[1]]).parents().slideDown();
 	}
 	
-	$('.container a').bind('click', function(e){
-		var a_link = $(this).attr('href');
-		if($(this).children().length == 0) {
+	aUnderContainer.bind('click', function(e){
+		var clikd_a = $(this);
+		var a_link = clikd_a.attr('href');
+
+		if(clikd_a.children().length == 0) {
+
 			e.preventDefault();
-			num = $('a').index(this);
+			num = aUnderContainer.index(clikd_a);
 			a_link += '?' + num;
 			window.open(a_link, '_self');
 		}
